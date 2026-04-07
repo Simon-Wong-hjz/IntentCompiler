727ced3a4683992131c4a5a0877566d606baa28d
Simon Wong <844621132@qq.com>
2026-04-07 21:18:58 +0800

    feat: add TaskSelector with responsive grid of 6 task type cards

---
+++ src/components/task-selector/TaskSelector.tsx
@@ -0,0 +1,29 @@
+import { TaskCard } from '@/components/task-selector/TaskCard';
+import { getAllTaskTypes } from '@/registry/template-registry';
+import type { TaskType } from '@/registry/types';
+
+interface TaskSelectorProps {
+  selectedType: TaskType | null;
+  onSelect: (type: TaskType) => void;
+}
+
+export function TaskSelector({ selectedType, onSelect }: TaskSelectorProps) {
+  const taskTypes = getAllTaskTypes();
+
+  return (
+    <div className="px-5 py-3">
+      <div className="grid grid-cols-6 gap-2 min-[0px]:max-[1279px]:grid-cols-3">
+        {taskTypes.map((tt) => (
+          <TaskCard
+            key={tt.type}
+            type={tt.type}
+            verb={tt.verb.en}
+            mentalModel={tt.mentalModel.en}
+            isSelected={selectedType === tt.type}
+            onClick={() => onSelect(tt.type)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
